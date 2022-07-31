512056631c065d85b02460b640d03ecf53d1db9b
Raj Nori <rajnoritech@yahoo.com>
2022-07-31 12:51:23 +1000

    graphql schema started

---
+++ server/sampleData.js
@@ -0,0 +1,72 @@
+const projects = [
+
+ {
+id: '1',
+clientId:'1',
+name:'Recipe website',
+Description:
+'A recipe website for a small company where the end user can login and check their recipes.'
+},
+{
+id: '2',
+clientId:'2',
+name:'Weather dashboard website',
+Description:
+'A weather dashboard website where business travellers can login and check the weather'
+},
+{
+ id: '3',
+clientId:'3',
+name:'Party planning website',
+Description:
+'A party planning website for a families where the end user can plan their parties for both parents and children.'
+},
+{
+id: '4',
+clientId:'4',
+name:'ecommerce website',
+Description:
+'An ecommerce website project where the users can log in and shop for their favourite goods online.'
+},
+{
+ id: '5',
+clientId:'5',
+name:'Booksearch',
+Description:
+'A book search engine where the users can search for books and film adaptations and check if any of the books were made into movies.'
+}
+]
+
+const clients = [
+  {
+    id: '1',
+    name: 'Homer Simpson',
+    email: 'homer@gmail.com',
+    phone: '555-555-5555',
+  },
+  {
+    id: '2',
+    name: 'Bart Simpson',
+    email: 'bart@gmail.com',
+    phone: '555-556-5555',
+  },
+
+  {
+    id: '3',
+    name: 'Marge Simpson',
+    email: 'marge@gmail.com',
+    phone: '555-666-6666',
+  },
+  {
+    id: '4',
+    name: 'Krusty',
+    email: 'krusty@gmail.com',
+    phone: '333-333-3335',
+  },
+  {
+    id: '5',
+    name: 'Burns',
+    email: 'burns@gmail.com',
+    phone: '555-555-0101',
+  },
+];
