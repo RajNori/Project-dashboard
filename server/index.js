03c1800cceb57084407eea03f638be411e788b03
Raj Nori <rajnoritech@yahoo.com>
2022-08-03 13:29:12 +1000

    changes for deployment

--- server/index.js
+++ server/index.js
@@ -1,35 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
-const cors = require ('cors');
-require('dotenv').config();
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema')
-const connectDB  = require('./config/db')
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 //path module for production/deploy to heroku 🚀
-const path = require('path')
-
+const path = require('path');
 
 const app = express();
 
 //connect
-connectDB()
+connectDB();
 
-app.use(cors())
+app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
- schema,
- graphiql: process.env.NODE_ENV === 'development'
-}))
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 //sttic folder for serving files in production/deploy to heroku 🚀 ⟠ 👇
 app.use(express.static('public'));
 
-app.get('*',(req,res)=>{
- res.sendFile(path.resolve(__dirname,'public','index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 // path resolve for serving the index file 🚀 ⟠ 👆
 const port = process.env.PORT || 5000;
 //deployment 🚀 ⟠ 👆
 
-
-app.listen(port, console.log(`Server running on ${port}`))
+app.listen(port, console.log(`Server running on ${port}`));
