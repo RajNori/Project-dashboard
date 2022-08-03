68dad910e0d590e6bcb1b625276710f627f87422
Raj Nori <rajnoritech@yahoo.com>
2022-08-03 13:16:37 +1000

    changed port for redeployment

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
  schema,
- graphiql: process.env.NODE_ENV === 'production'
+ graphiql: process.env.NODE_ENV === 'development'
 }))
 
 //sttic folder for serving files in production/deploy to heroku 🚀 ⟠ 👇
@@ -28,6 +28,8 @@ app.get('*',(req,res)=>{
  res.sendFile(path.resolve(__dirname,'public','index.html'))
 })
 // path resolve for serving the index file 🚀 ⟠ 👆
-const port = process.env.PORT || 5000;
+const port = process.env.PORT 
+//deployment 🚀 ⟠ 👆
+// || 5000;
 
 app.listen(port, console.log(`Server running on ${port}`))
