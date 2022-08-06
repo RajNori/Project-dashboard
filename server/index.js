ed7aca3aacc609094c31813a4ac73cabb8d40cfe
Raj Nori <rajnoritech@yahoo.com>
2022-08-06 22:30:07 +1000

    feature branch started

--- server/index.js
+++ server/index.js
@@ -15,14 +15,6 @@ connectDB();
 
 app.use(cors());
 
-//from express-graphql, recommended settings for production👇  
-// app.use('/graphql', graphqlHTTP({
-//   schema:schema,
-//   rootvalue:root,
-//   graphiql:true,
-//   })
-// );
-
 // My settings👇
 app.use(
   '/graphql',
@@ -35,23 +27,8 @@ app.use(
 //static folder for serving files in production/deploy to heroku 🚀 ⟠ 👇
 app.use(express.static('client/build'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-// });
-
-// List of all the files that should be served as-is
-let protected = ['transformed.js', 'main.css', 'favicon.ico']
-
-app.get("*", (req, res) => {
-
-  let path = req.params['0'].substring(1)
-   if (protected.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
-  }
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
 });
 // path resolve for serving the index file 🚀 ⟠ 👆
 const PORT = process.env.PORT || 5000;
